bc3d53a0d9d39dc7804be3a0ea16b194e0f5f9f2
Daniel Bowen <djbowen95@gmail.com>
2022-01-24 13:08:14 +0000

    push today's date to header when page loads

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-// Code here.
+const currentDayDisplay = document.getElementById("current-day");
+
+// Function to write date in header.
+function writeDate () {
+    currentDayDisplay.innerText = moment().format('dddd Do MMMM YYYY');
+}
+
+writeDate();

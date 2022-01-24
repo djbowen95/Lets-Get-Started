9b5309d70700b6bd741e980482d92b41384f4834
Daniel Bowen <djbowen95@gmail.com>
2022-01-24 14:51:35 +0000

    track time with simple interval element and moment.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 const currentDayDisplay = document.getElementById("current-day");
+currentDayDisplay.innerText = moment().format('dddd Do MMMM YYYY');
 
-// Function to write date in header.
-function writeDate () {
-    currentDayDisplay.innerText = moment().format('dddd Do MMMM YYYY');
-}
+const momentTester = document.getElementById('moment-test-element');
 
-writeDate();
+const liveTime = setInterval(function() {
+    const currentSecond = moment().format('h:mm:ss a');
+    momentTester.innerText = `${currentSecond}`
+}, 1000);

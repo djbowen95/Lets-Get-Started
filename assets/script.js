cf3a11d97a50010d9736141d27689993d998fb61
Daniel Bowen <djbowen95@gmail.com>
2023-02-06 19:06:27 +0000

    Comment up existing script.js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
+// Displays current date in the Header element.
 const currentDayDisplay = document.getElementById("current-day");
 currentDayDisplay.innerText = moment().format('dddd Do MMMM YYYY');
 
+// Using this for displaying the current time.
 const momentTester = document.getElementById('moment-test-element');
 
+// This refreshes the page every second.
+// Might not be the best way to do this! 
 const liveTime = setInterval(function() {
     const currentSecond = moment().format('HH:mm:ss');
     const currentHour = parseInt(moment().format('HH'));
+
+    // Updates every second with current time.
     momentTester.innerText = `${currentSecond}`;
 
+    // I'm not sure if I like this/might scrap it.
     if (currentHour < 9) {
         document.getElementById('prehours').style.backgroundColor = "Blue";
     } else if (currentHour > 17) {
@@ -24,6 +31,8 @@ const liveTime = setInterval(function() {
 /*
 // while (moment().format('HH') <= 9)
 */
+// Currently just making 3 o'clock green.
+// Should do this by applying a class. 
 function checkHour() {
     if (moment().format('HH') >= 15) {
         document.getElementById('hour15').style.backgroundColor = "Green";

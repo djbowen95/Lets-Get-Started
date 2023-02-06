f450726fe3ab7bd640b6f32a171dd8493a076b0d
Daniel Bowen <djbowen95@gmail.com>
2023-02-06 19:46:16 +0000

    Have backgrounds change depending on hour retrieved from moment.js

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,49 @@
 // Displays current date in the Header element.
 const currentDayDisplay = document.getElementById("current-day");
 // Using this for displaying the current time.
-const momentTester = document.getElementById('moment-test-element');
+const momentTester = document.getElementById("moment-test-element");
 
 // This refreshes the page every second.
-// Might not be the best way to do this! 
-const liveTime = setInterval(function() {
-    const currentSecond = moment().format('HH:mm:ss');
-    const currentHour = parseInt(moment().format('HH'));
+// Might not be the best way to do this!
+const liveTime = setInterval(function () {
+  const currentSecond = moment().format("HH:mm:ss");
+  const currentHour = parseInt(moment().format("HH"));
 
-    // Updates every second with current time.
-    momentTester.innerText = `${currentSecond}`;
+  // Updates every second with current time.
+  momentTester.innerText = `${currentSecond}`;
 }, 1000);
 
 // Should do this by applying a class.
 // Must be called last.
 function setCurrentHourBackground(hour) {
-    document.getElementById(`tb-hour-${hour}`).style.backgroundColor = "Green";
+  document.getElementById(`tb-hour-${hour}`).style.backgroundColor = "Green";
 }
 
 function setPastHourBackground(hour) {
-    for (let i = 9; i <= hour; i++) {
-        document.getElementById(`tb-hour-${i}`).style.backgroundColor = "Red";
-    }
+  for (let i = 9; i <= hour; i++) {
+    document.getElementById(`tb-hour-${i}`).style.backgroundColor = "Red";
+  }
 }
 
 function setUpcomingHourBackground(hour) {
-    for (let i = 17; i >= hour; i--) {
-        document.getElementById(`tb-hour-${i}`).style.backgroundColor = "Orange";
-    }
+  for (let i = 17; i >= hour; i--) {
+    document.getElementById(`tb-hour-${i}`).style.backgroundColor = "Orange";
+  }
 }
 
+function setTimeblockBackgrounds(hour) {
+  setUpcomingHourBackground(hour);
+  setPastHourBackground(hour);
+  setCurrentHourBackground(hour);
+}
+
+function getHour() {
+    let currentHour = moment().format("HH");
+    let intHour = parseInt(currentHour) - 7; // Minus 7 to test for now.
+    return intHour;
+}
 // Updates the date on page refresh.
-currentDayDisplay.innerText = moment().format('dddd Do MMMM YYYY');
+currentDayDisplay.innerText = moment().format("dddd Do MMMM YYYY");
 
-
-setUpcomingHourBackground(12);
-setPastHourBackground(12);
-setCurrentHourBackground(12);
-console.log(parseInt(moment().format('HH')) + 6)
+getHour();
+setTimeblockBackgrounds(getHour());
